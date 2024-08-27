2a497cbca431d26b8e348a2179a585f5e32bb9c7
xzl666 <118340974+18979705623@users.noreply.github.com>
2024-08-27 23:25:05 +0800

    Add files via upload

--- pretrain_auxiliary_src/model/pretrain_cmt.py
+++ pretrain_auxiliary_src/model/pretrain_cmt.py
@@ -1,4 +1,5 @@
 import json
+import math
 from collections import defaultdict
 
 import numpy as np
@@ -41,25 +42,8 @@ class ClsPrediction(nn.Module):
         return self.net(x)
 
 
-class AttentionPrediction(nn.Module):
-    def __init__(self, hidden_size, input_size=None):
-        super().__init__()
-        if input_size is None:
-            input_size = hidden_size
-        self.query = nn.Linear(input_size, 1)
-        self.key = nn.Linear(input_size, 1)
-
-    def forward(self, x):
-        query = self.query(x)
-        key = self.key(x)
-        scores = query * key
-        attention_weights = F.softmax(scores, dim=0)
-        output = attention_weights.mean()
-        return output
-
-
 class TransformerPrediction(nn.Module):
-    def __init__(self, config, label_length=31, num_layers=2, num_heads=8, hidden_size=768):
+    def __init__(self, config, feature_length=39, label_length=31, num_layers=2, num_heads=8, hidden_size=768):
         super(TransformerPrediction, self).__init__()
         self.linear = nn.Linear(config.hidden_size, hidden_size)
         self.transformer_encoder_layer = TransformerEncoderLayer(hidden_size, num_heads)
@@ -101,8 +85,6 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
         if 'sap' in config.pretrain_tasks:
             self.global_sap_head = ClsPrediction(self.config.hidden_size)
             self.local_sap_head = ClsPrediction(self.config.hidden_size)
-            self.activ_thold_liner = AttentionPrediction(self.config.hidden_size)
-            self.conn_scale_liner = AttentionPrediction(self.config.hidden_size, input_size=self.config.hidden_size * 2)
             if config.glocal_fuse:
                 self.sap_fuse_linear = ClsPrediction(self.config.hidden_size, input_size=self.config.hidden_size * 2)
             else:
@@ -148,6 +130,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
         return try_cuda(all_list)
 
     def get_room_type_for_end(self, scan, cand_vpids):
+        " 得到基于视觉的房间分类 "
         all_list = torch.LongTensor([])
         for i, ViewpointId in enumerate(cand_vpids):
             room_type = torch.LongTensor([self.room_labels[scan[i]][ViewpointId]])  # int——>tensor
@@ -155,7 +138,10 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
         return try_cuda(all_list)
 
     def get_room_type_for_view(self, scan, cand_vpids):
+        " 得到基于视觉的房间分类 "
+        # print('room_labels:', self.room_labels['EDJbREhghzL']['a1dd7b2789d64b5a8e9adcd864ad9897'])
         all_list = torch.LongTensor([])
+        # print(all_list.device) #cpu
         max_num_a = 0
         for i, cand in enumerate(cand_vpids):
             max_num_a = max(max_num_a, len(cand))
@@ -165,13 +151,12 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
                 room_type = torch.LongTensor([self.room_labels[scan[i]][ViewpointId]])  # int——>tensor
                 list_view = torch.cat([list_view, room_type], 0)
             while len(list_view) < max_num_a:
-                list_view = torch.cat([list_view, torch.tensor([30])], 0)  # fill
+                list_view = torch.cat([list_view, torch.tensor([30])], 0)  # 填充
             all_list = torch.cat([all_list, list_view], 0)
         return try_cuda(all_list)
 
     def get_reasoning_conn(self, view_label_class, text_label_class, batch_size, action_count, visited_masks=None):
-        '''Calculate the proximity between the currently seen room type and the target room type,
-        using the prior knowledge self.room_class_relation.'''
+        "计算当前看到的房间类型和目标房间类型的连通概率，使用先验知识self.room_class_relation"
         text_label_class = text_label_class[:, None, :].expand(-1, action_count, -1). \
             reshape(action_count * batch_size, -1)
         gamma = 0.9
@@ -248,14 +233,9 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             )
         elif task.startswith('rc'):
             return self.forward_rc(
-                batch['txt_ids'], batch['txt_lens'], batch['traj_view_img_fts'],
-                batch['traj_obj_img_fts'], batch['traj_loc_fts'], batch['traj_nav_types'],
-                batch['traj_step_lens'], batch['traj_vp_view_lens'], batch['traj_vp_obj_lens'],
-                batch['traj_vpids'], batch['traj_cand_vpids'],
-                batch['gmap_lens'], batch['gmap_step_ids'], batch['gmap_pos_fts'],
-                batch['gmap_pair_dists'], batch['gmap_vpids'], batch['vp_pos_fts'],
-                batch['vp_view_mrc_masks'], batch['vp_view_probs'],
-                batch['vp_obj_mrc_masks'], batch['vp_obj_probs'], batch['obj_labels'], batch['scan'], compute_loss
+                batch['traj_view_img_fts'],
+                batch['traj_obj_img_fts'],
+                batch['traj_vpids'], batch['scan'], compute_loss
             )
         elif task.startswith('mrc'):
             return self.forward_mrc(
@@ -277,7 +257,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
                 batch['gmap_lens'], batch['gmap_step_ids'], batch['gmap_pos_fts'],
                 batch['gmap_pair_dists'], batch['gmap_vpids'], batch['vp_pos_fts'],
                 batch['gmap_visited_masks'],
-                batch['global_act_labels'], batch['local_act_labels'], batch['end_vpids'], batch['scan'], compute_loss
+                batch['global_act_labels'], batch['local_act_labels'], compute_loss
             )
         elif task.startswith('og'):
             return self.forward_og(
@@ -362,7 +342,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
         traj_obj_img_fts = self.pad_tensors(traj_obj_img_fts, traj_view_img_fts.shape)
         region_fts = torch.cat([traj_view_img_fts, traj_obj_img_fts], 1)
 
-        region_soft_labels = self.region_classifier(region_fts)
+        region_soft_labels = self.region_classifier(region_fts)  # .reshape(region_fts.shape[0], -1)
         region_targets = self._compute_gt_labels(traj_vpids, scan)
         if compute_loss:
             rc_loss = F.cross_entropy(region_soft_labels, region_targets, reduction='none')
@@ -425,7 +405,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             self, txt_ids, txt_lens, traj_view_img_fts, traj_obj_img_fts, traj_loc_fts, traj_nav_types,
             traj_step_lens, traj_vp_view_lens, traj_vp_obj_lens, traj_vpids, traj_cand_vpids,
             gmap_lens, gmap_step_ids, gmap_pos_fts, gmap_pair_dists, gmap_vpids, vp_pos_fts,
-            gmap_visited_masks, global_act_labels, local_act_labels, end_vpids, scan, compute_loss,
+            gmap_visited_masks, global_act_labels, local_act_labels, compute_loss,
     ):
         batch_size = txt_ids.size(0)
 
@@ -434,10 +414,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             traj_step_lens, traj_vp_view_lens, traj_vp_obj_lens, traj_vpids, traj_cand_vpids,
             gmap_lens, gmap_step_ids, gmap_pos_fts, gmap_pair_dists, gmap_vpids, vp_pos_fts,
         )
-        activate_threshold = torch.abs(self.activ_thold_liner(torch.ones_like(vp_embeds[:, 0])))
-        activate_threshold[activate_threshold < 0.7] = 0.7
-        conn_scaling_weights = torch.abs(self.conn_scale_liner(torch.cat([gmap_embeds[:, 0], vp_embeds[:, 0]], 1)))
-        conn_scaling_weights[conn_scaling_weights < 1.25] = 1.25
+
         if self.sap_fuse_linear is None:
             fuse_weights = 0.5
         else:
@@ -448,23 +425,6 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
         global_logits = self.global_sap_head(gmap_embeds).squeeze(2) * fuse_weights
         global_logits.masked_fill_(gmap_visited_masks, -float('inf'))
         global_logits.masked_fill_(gen_seq_masks(gmap_lens).logical_not(), -float('inf'))
-        end_room_type = self.get_room_type_for_end(scan, end_vpids)
-        gmap_vpid = gmap_vpids.copy()
-        for i in range(batch_size):
-            del gmap_vpid[i][0]
-        global_room_type = self.get_room_type_for_view(scan, gmap_vpid).reshape(batch_size, -1)
-        global_view_labels_oh = try_cuda(torch.zeros(global_room_type.shape[0] * global_room_type.shape[1], 31))
-        global_view_labels_oh.scatter_(1, global_room_type.reshape(-1)[:, None], 1)
-        end_room_type_oh = try_cuda(torch.zeros(batch_size, 31))
-        end_room_type_oh.scatter_(1, end_room_type[:, None], 1)
-        global_conn = self.get_reasoning_conn(global_view_labels_oh, end_room_type_oh, batch_size,
-                                              global_room_type.shape[1],
-                                              gmap_visited_masks[:, 1:])
-        global_conn = self.activate_function(global_conn, activate_threshold, conn_scaling_weights)
-        global_conn = torch.cat([torch.zeros(batch_size, 1).cuda(), global_conn], 1)
-        global_conn.masked_fill_(gmap_visited_masks, 0)
-        global_conn.masked_fill_(gen_seq_masks(gmap_lens).logical_not(), 0)
-        fused_global_logits = global_logits + global_conn
 
         local_logits = self.local_sap_head(vp_embeds).squeeze(2) * (1 - fuse_weights)
         vp_nav_masks = pad_tensors_wgrad(
@@ -474,52 +434,25 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             [torch.zeros(len(vp_nav_masks), 1).bool().to(vp_nav_masks.device), vp_nav_masks], 1
         )  # add [stop]
         local_logits.masked_fill_(vp_nav_masks, -float('inf'))
-        vp_cand_vpids = []
-        for i in range(batch_size):
-            vp_cand_vpids.append(traj_cand_vpids[i][-1])
-        local_room_type = self.get_room_type_for_view(scan, vp_cand_vpids).reshape(batch_size, -1)
-        local_view_labels_oh = try_cuda(torch.zeros(local_room_type.shape[0] * local_room_type.shape[1], 31))
-        local_view_labels_oh.scatter_(1, local_room_type.reshape(-1)[:, None], 1)
-        local_conn = self.get_reasoning_conn(local_view_labels_oh, end_room_type_oh, batch_size,
-                                             local_room_type.shape[1])
-        local_conn = self.activate_function(local_conn, activate_threshold, conn_scaling_weights)
-        local_conn.masked_fill_(vp_nav_masks[:, 1:local_conn.shape[1] + 1], 0)
-        fused_local_logits = torch.clone(local_logits)
-        fused_local_logits[:, 1:local_conn.shape[1] + 1] += local_conn
 
-        # residual fusion
+        # fusion
         fused_logits = torch.clone(global_logits)
-        all_fused_logits = torch.clone(fused_global_logits)
         fused_logits[:, 0] += local_logits[:, 0]  # stop
-        all_fused_logits[:, 0] += fused_local_logits[:, 0]
-
         for i in range(batch_size):
             visited_nodes = set([vp for vp, mask in zip(gmap_vpids[i], gmap_visited_masks[i]) if mask])
-            tmp_0 = {}
-            tmp_1 = {}
-            bw_logits0 = 0
-            bw_logits1 = 0
+            tmp = {}
+            bw_logits = 0
             for j, cand_vpid in enumerate(traj_cand_vpids[i][-1]):
                 if cand_vpid in visited_nodes:
-                    bw_logits0 += local_logits[i, j + 1]
-                    bw_logits1 += fused_local_logits[i, j + 1]
+                    bw_logits += local_logits[i, j + 1]
                 else:
-                    tmp_0[cand_vpid] = local_logits[i, j + 1]
-                    tmp_1[cand_vpid] = fused_local_logits[i, j + 1]
+                    tmp[cand_vpid] = local_logits[i, j + 1]
             for j, vp in enumerate(gmap_vpids[i]):
                 if j > 0 and vp not in visited_nodes:
-                    if vp in tmp_0:
-                        fused_logits[i, j] += tmp_0[vp]
-                        all_fused_logits[i, j] += tmp_1[vp]
+                    if vp in tmp:
+                        fused_logits[i, j] += tmp[vp]
                     else:
-                        fused_logits[i, j] += bw_logits0
-                        all_fused_logits[i, j] += bw_logits1
-
-        stop_singals = [torch.argmax(fused_logit) == 0 for fused_logit in fused_logits]
-        for i, stop_singal in enumerate(stop_singals):
-            # Stop according to visual logits.
-            if stop_singal:
-                all_fused_logits[i, 0] += 99
+                        fused_logits[i, j] += bw_logits
 
         if compute_loss:
             global_losses = F.cross_entropy(global_logits, global_act_labels, reduction='none')
@@ -528,7 +461,7 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             losses = global_losses + local_losses + fused_losses
             return losses
         else:
-            return global_logits, local_logits, fused_logits, all_fused_logits, global_act_labels, local_act_labels
+            return global_logits, local_logits, fused_logits, global_act_labels, local_act_labels
 
     def forward_og(
             self, txt_ids, txt_lens, traj_view_img_fts, traj_obj_img_fts, traj_loc_fts, traj_nav_types,
@@ -564,9 +497,10 @@ class GlocalTextPathCMTPreTraining(BertPreTrainedModel):
             self, txt_ids, txt_lens, traj_view_img_fts, traj_obj_img_fts, traj_loc_fts, traj_nav_types,
             traj_step_lens, traj_vp_view_lens, traj_vp_obj_lens, traj_vpids, traj_cand_vpids,
             gmap_lens, gmap_step_ids, gmap_pos_fts, gmap_pair_dists, gmap_vpids, vp_pos_fts,
-            gmap_visited_masks
+            gmap_visited_masks, global_act_labels, local_act_labels, obj_labels
     ):
         batch_size = txt_ids.size(0)
+        print('true sap og')
 
         gmap_embeds, vp_embeds = self.bert(
             txt_ids, txt_lens, traj_view_img_fts, traj_obj_img_fts, traj_loc_fts, traj_nav_types,
